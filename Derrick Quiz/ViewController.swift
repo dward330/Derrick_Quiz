39c9e59ac295346cc66607c741fa979828822c75
Derrick Ward <Derrick@derricks-macbook-pro.local>
2016-10-20 20:51:33 -0400

    Initial Commit

---
+++ Derrick Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Derrick Quiz
+//
+//  Created by Derrick Ward on 10/20/16.
+//  Copyright © 2016 Derrick Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
