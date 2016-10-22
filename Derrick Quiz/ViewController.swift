867c3d9f219d44423df3b259d31a6b5c7da13ad5
Derrick Ward <Derrick@derricks-macbook-pro.local>
2016-10-22 18:28:40 -0400

    Author: Derrick Ward
    Changes:
    -Buttons and Labels are Linked.
    -Base App Functionality is Done

--- Derrick Quiz/ViewController.swift
+++ Derrick Quiz/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel : UILabel!
+    @IBOutlet var answerLabel : UILabel!
+    
+    let questions : [String] = ["What is Derrick's Middle Name?",
+                                "What college did Derrick go to?",
+                                "What was Derrick's Major?"]
+    let answers : [String] = ["Kyle","University of Pittsburgh","Computer Engineering"]
+    
+    var questionIndex : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        questionLabel.text=questions[0] //Show the first question
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Operations to be completed when showNextButton is clicked
+    @IBAction func showNextQuestion(sender:AnyObject){
+        questionIndex += 1 //increment question index counter
+        
+        //Figure out if we need to restart the question counter
+        if (questionIndex == questions.count){
+            questionIndex = 0
+        }
+        
+        //Load the next question
+        questionLabel.text = questions[questionIndex]
+    }
+    
+    //Operations to be completed when showAnswer is clicked
+    @IBAction func showAnswer(sender:AnyObject){
+        answerLabel.text=answers[questionIndex]
+    }
 
 }
 
